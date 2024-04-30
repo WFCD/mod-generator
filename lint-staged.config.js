38e143498ce09a30f778df0f48c1d37538f785b9
Matej Voboril <tobiah@pm.me>
2024-04-29 21:51:48 -0500

    ci: more linting, configs to standalone files

---
+++ lint-staged.config.js
@@ -0,0 +1,15 @@
+export default {
+  "*.ts": [
+    "eslint --cache --fix",
+    "prettier --write",
+    () => "tsc -p tsconfig.json --noEmit",
+    () => "npm test"
+  ],
+  "*.{json,yaml,yml,md},!package*.json": [
+    "prettier --write"
+  ],
+  "package*.json": [
+    "prettier --write",
+    "npm -q --no-progress dedupe"
+  ]
+}
